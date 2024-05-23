b0cce63c116930205cc307df59ae8dc04992b548
Somil Bichpuriya <57514538+sbichpuriya11@users.noreply.github.com>
2024-05-23 09:21:33 +0530

    setting up new project template for e-commerce

---
+++ nodejs-complete-guide/products.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .send('<h3 style="font-family:sf pro display">Page not found</h3>');
+});
+
+//should go at last after defining and setting up all routes/models and code logic
+app.listen(3000);
