7272904fc0d1ea7b64dd72817ac7469673ea7494
Somil Bichpuriya <57514538+sbichpuriya11@users.noreply.github.com>
2024-05-23 09:03:50 +0530

    seggregating similar routes to different files

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   res.send(
-    `<form style='font-family:sf pro display' action="/product" method="POST"><label>Product Title</label><br/><input type="text" name="title"/><button type="submit">Add Product</button></form>`
+    `<form style='font-family:sf pro display' action="/admin/add-product" method="POST"><label>Product Title</label><br/><input type="text" name="title"/><button type="submit">Add Product</button></form>`
   );
 });
 
-router.post("/product", (req, res, next) => {
+///admin/add-product =>POST
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
