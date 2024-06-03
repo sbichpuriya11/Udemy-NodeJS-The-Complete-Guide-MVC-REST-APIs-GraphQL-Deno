f5eadb61b2f4ad909f2fc184f0a2379336b0306d
Somil Bichpuriya <57514538+sbichpuriya11@users.noreply.github.com>
2024-06-03 07:30:41 +0530

    code refactoring for model class

--- ejs_mvc/model/product.js
+++ ejs_mvc/model/product.js
@@ -2,6 +2,15 @@ const products = [];
 const fs = require("fs");
 const path = require("path");
 const root_path = require("../utils/path");
+
+const filePath = path.join(root_path, "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(filePath, (err, fileContent) => {
+    if (err) return cb([]);
+    cb(JSON.parse(fileContent));
+  });
+};
 module.exports = class Product {
   constructor(productName, productPrice, productImage, productDescription) {
     this.productName = productName;
@@ -11,26 +20,15 @@ module.exports = class Product {
   }
 
   save() {
-    const filePath = path.join(root_path, "data", "products.json");
-    fs.readFile(filePath, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(filePath, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
-    products.push(this);
   }
 
   static fetchAll(cb) {
-    const filePath = path.join(root_path, "data", "products.json");
-    fs.readFile(filePath, (err, fileContent) => {
-      if (err) cb([]);
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
